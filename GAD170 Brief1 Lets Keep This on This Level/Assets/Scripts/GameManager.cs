fbf09e031d30042e0da819f8c94e77554f1b5250
1019351 <61863245+1019351@users.noreply.github.com>
2020-03-26 13:09:00 +0800

    GAD170Brief1

---
+++ GAD170 Brief1 Lets Keep This on This Level/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Properties
+    public static int score;        // The number of coins collected. 
+                                    // NOTE the 'static' keyword here. This means it can be accessed from ANY script by calling GameManager.score (Eg see Coin script)
+                                    // NOTE: 'static' properties do NOT show in the Inspector.
+
+    public int targetScore = 10;    // The number of coins to collect.
+
+    // Methods
+    private void Update()
+    {
+        // Check the number of coins that have been collected.
+        if (GameManager.score == this.targetScore)
+        {
+            // Output that the number of target coins have been collected.
+            Debug.Log("Player has collected " + this.targetScore + " coins!");
+
+            // Deactivate this GameManager component so that it stops checking the score each Update().
+            this.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ GAD170 Brief1 Lets Keep This on This Level/Assets/Scripts/ResetLevel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetLevel : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) == true) { SceneManager.LoadScene(0); }
+    }
+}
